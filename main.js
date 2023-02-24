b82ef626a667cca7ab5de3b6c42ed3f587e656b1
taetoo <rlaougs5@naver.com>
2023-02-24 10:39:31 +0900

    Add: Map 객체 변경

--- main.js
+++ main.js
@@ -9,17 +9,28 @@ toggleBtn.addEventListener('click', () => {
 })
 
 
+// const fileInput = document.getElementById("chooseFile");
+// // 또는 const fileInput = $("#fileUpload").get(0);
+
+// fileInput.onchange = () => {
+//   const selectedFile = [...fileInput.files];
+//   console.log(selectedFile);
+// };
+var num = 0;
+var filesArr = new Map();
 
 // 파일업로드
 function DropFile(dropAreaId, fileListId) {
     let dropArea = document.getElementById(dropAreaId);
     let fileList = document.getElementById(fileListId);
-  
+
+    // 기존 브라우저 drag&drop 이벤트 막기
     function preventDefaults(e) {
       e.preventDefault();
       e.stopPropagation();
     }
-  
+    
+    // drop 파일 영역에 하이라트 효과
     function highlight(e) {
       preventDefaults(e);
       dropArea.classList.add("highlight");
@@ -30,26 +41,33 @@ function DropFile(dropAreaId, fileListId) {
       dropArea.classList.remove("highlight");
     }
   
+
+    // drop 파일 영역에 파일을 내려놨을 때 생기는 이벤트
     function handleDrop(e) {
       unhighlight(e);
       let dt = e.dataTransfer;
       let files = dt.files;
-  
       handleFiles(files);
-  
+      console.log(files)
       const fileList = document.getElementById(fileListId);
       if (fileList) {
         fileList.scrollTo({ top: fileList.scrollHeight });
       }
     }
-  
+
     function handleFiles(files) {
-      files = [...files];
-      files.forEach(previewFile);
+      let copy = [...files];
+      copy.forEach(previewFile)
+      copy.forEach(setMap)
+      console.log(filesArr);
     }
-  
+
+    function setMap(file){
+      filesArr.set(num,file);
+      num++;
+    }
+
     function previewFile(file) {
-      console.log(file);
       fileList.appendChild(renderFile(file));
     }
   
@@ -72,7 +90,7 @@ function DropFile(dropAreaId, fileListId) {
             <span class="percent">100% done</span>
             <span class="speed">90KB/sec</span>
           </div> 
-          <a class="delete"><i class="fas fa-minus-square" ></i></a>
+          <a class="delete"><i onclick='remove_children()' class="fas fa-minus-square" ></i></a>
         </div>
       `;
       return fileDOM;
@@ -92,13 +110,17 @@ function DropFile(dropAreaId, fileListId) {
 
 
 function remove_children() {
-    const parent = document.getElementById('files')
-
     const child = document.getElementsByClassName('file')
-
+    child.parentNode.removeChild(child);
     
+    // const parent = document.getElementById('files')
+    // parent.removeChild(child.num)
+    // filesArr.delete(num)
 
-    child.innerHTML.remove();
+
+    // $('#file' + num).remove();
+    // child.delete(num);
+    // console.log(child)
 } 
 
 
